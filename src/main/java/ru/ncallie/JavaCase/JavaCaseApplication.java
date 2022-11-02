11d07db337610f4f1d29d9513bbe45092f4b5a37
Roman <romanbinel@vk.com>
2022-11-02 13:44:45 +0300

    Initial commit

---
+++ src/main/java/ru/ncallie/JavaCase/JavaCaseApplication.java
@@ -0,0 +1,13 @@
+package ru.ncallie.JavaCase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ncallie/JavaCase/JavaCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ncallie.JavaCase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
